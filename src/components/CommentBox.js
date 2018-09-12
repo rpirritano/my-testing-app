2832aa8cfde53692b1fc492baaf393690f1c345c
Robert Pirritano <rpirritano@gmail.com>
2018-09-12 13:30:10 -0700

    setp up redux with actions and reducer

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = {
@@ -12,7 +13,8 @@ class CommentBox extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    //// TODO: call an action creator and save the comment
+    //action creator
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: ''});
   }
 
@@ -40,4 +42,4 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
